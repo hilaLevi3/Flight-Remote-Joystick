1dd1f6b2c98beae49082e8bd1cf2ae7fa4236a8c
Lior Levi <lior.levi5@live.biu.ac.il>
2021-06-22 18:15:19 +0300

    second try

--- app/src/main/java/com/example/try12/Model.kt
+++ app/src/main/java/com/example/try12/Model.kt
@@ -1,2 +1,23 @@
 package com.example.try12
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.ServerSocket
+import java.net.Socket
+import java.io.PrintWriter
 
+fun main () {
+    connect()
+}
+
+// this will connect to the server (which is the flight gear)
+fun connect() {
+    val client = Socket("127.0.0.1", 6400)
+    val output = PrintWriter(client.getOutputStream(), true)
+    val input = BufferedReader(InputStreamReader(client.inputStream))
+
+    println("Client sending [Hello]")
+    output.println("set /controls/flight/aileron 0.5")
+    output.flush()
+    println("Client receiving [${input.readLine()}]")
+    client.close()
+}
